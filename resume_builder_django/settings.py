40f7ffb95f41c363c4b85b6dc3fdce7e56afb38c
Rutuj <61797632+rutuj24@users.noreply.github.com>
2022-12-21 23:03:34 +0530

    Update settings.py

--- resume_builder_django/settings.py
+++ resume_builder_django/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+j!!$v&_ia)q1hughf6uog(d*nn!rus=_m-wq^(4a)eq#gyxco'
+SECRET_KEY = 'Your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
